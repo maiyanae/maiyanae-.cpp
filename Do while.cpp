ab0dc52ae9dcf8785335886dccf5df0ee0c02ae4
maiyanae <113591816+maiyanae@users.noreply.github.com>
2022-10-10 10:48:46 -0700

    Create Do while.cpp

---
+++ Do while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	char input;
+	do {
+		cout <<"welcome to kirinyaga university"<<endl;
+		cout <<"enter k to see more about kirinyaga"<<endl;
+		cin>> input;
+	}while( input!= 'k');
+	return 0;
+}
+		
+		
+		
