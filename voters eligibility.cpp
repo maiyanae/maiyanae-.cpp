69bb5ece458439ed2abe4a8b5c2ace4bbbd95335
maiyanae <113591816+maiyanae@users.noreply.github.com>
2022-10-10 11:48:52 -0700

    Create voters eligibility.cpp

---
+++ voters eligibility.cpp
@@ -0,0 +1,24 @@
+// program to check voters eligibility
+ # include <iostream>
+  
+  using namespace std;
+  int main ()
+  {
+  	
+   int age;
+  cout << "enter your age" << endl;
+  cin >> age;
+  
+  if (age >= 18) {
+  
+  	cout << "you are eligible to vote" << endl;
+	
+  
+  } else {
+  
+  cout << "not eligible. "; }	
+  	
+	  
+	  return 0;
+	  
+	  }
